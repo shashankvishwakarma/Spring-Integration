614fb9576268841021a0cfb5e59d929027c3fffa
shashankvishwakarma <50291242+shashankvishwakarma@users.noreply.github.com>
2019-06-16 19:04:52 +0530

    Initial Commit to demonstrate Spring Integration

---
+++ src/main/java/com/example/spring/integration/config/FileIntegrationConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.spring.integration.config;
+
+import com.example.spring.integration.Transformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
+import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
+
+import java.io.File;
+
+@Configuration
+public class FileIntegrationConfiguration {
+
+    @Autowired
+    Transformer transformer;
+
+    @Bean
+    public IntegrationFlow integrationFlow() {
+        return IntegrationFlows.from(fileReader(),
+                spec -> spec.poller(Pollers.fixedDelay(500)))
+                .transform(transformer, "transform")
+                .handle(fileWriter())
+                .get();
+    }
+
+    private FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(
+                new File("destination")
+        );
+        handler.setAutoCreateDirectory(true);
+        handler.setExpectReply(false);
+        return handler;
+    }
+
+    private FileReadingMessageSource fileReader() {
+        FileReadingMessageSource source = new FileReadingMessageSource();
+        source.setDirectory(new File("source"));
+        source.setAutoCreateDirectory(true);
+        return source;
+    }
+}
